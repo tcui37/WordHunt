c780407b46e7d365ce2ef7eec976f5362cb56d91
Tony Cui <tacui@mit.edu>
2022-06-21 22:22:26 -0700

    sofar

--- wordhunt.py
+++ wordhunt.py
@@ -1,3 +1,5 @@
+from english_words import english_words_lower_alpha_set
+
 # Input: array of characters of length 16
 # output: a 4x4 array
 def arr_to_grid(str):
@@ -11,7 +13,66 @@ def arr_to_grid(str):
         grid[row][col] = c
     return grid
 
+# returns all words from (i,j) with seen {}
+# cord: i,j
+# seen: set of cord
+# curr_path: list of cord
+# paths: list of paths
+def all_paths(cord,grid,seen=set(),curr_path=[],paths=[],limit=8):
+    seen.add(cord)
+    if len(curr_path) >= 3:
+        paths.append(curr_path)
+    for neighbor in get_neighbors(cord):
+        # skip neighbor if its in our path
+        if neighbor in seen:
+            continue
+        if len(curr_path) > 8:
+            continue
+        all_paths(neighbor,grid,seen.copy(),curr_path + [neighbor],paths)
+
+
+#returns set of 8 neighboring cords of cord (i,j)
+def get_neighbors(cord):
+    return_set = set()
+    for i in (-1, 0, 1):
+        for j in (-1, 0, 1):
+            new_cord = (min(3,(max(0,cord[0] + i))), min(3,(max(0,cord[1] + j))))
+            return_set.add(new_cord)
+    return return_set
+    
+# input: path of cords, 4x4 grid
+def path_to_word(path,grid):
+    str = ""
+    for row,col in path:
+        str += grid[row][col]
+    return str
+
+#input 4x4 2d array of lowercase characters
+def generate_words(str):
+    grid = arr_to_grid(str)
+    paths = []
+    word_list = []
+    for i in range(4):
+        for j in range(4):
+            all_paths((i,j),grid,set(),[],paths)
+    for path in paths:
+        str = path_to_word(path,grid)
+        if str in english_words_lower_alpha_set:
+            word_list.append(str)
+    word_list = list(set(word_list))
+    word_list.sort(reverse=True, key=len)
+    return word_list
+
+
 
 if __name__ == '__main__':
-    str = "VMFWCUWLREHAREEG"
-    print(arr_to_grid(str))
+    str = "wfgbtmiarhdiafom".lower()
+    # grid = arr_to_grid(str)
+    # paths = []
+    # start = (0,0)
+    # all_paths(start,grid,set(),[],paths)
+    # path1 = paths[4]
+    # print(path_to_word(path1,grid))
+    print(generate_words(str))
+    # print("here" in english_words_lower_alpha_set)
+    pass
